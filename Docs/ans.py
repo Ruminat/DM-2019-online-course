62623bc131a8c2e76a7fcc439bdcf98959a787e9
Ruminat <Blowlyge@gmail.com>
2019-04-09 21:25:40 +0300

    Turned it to a web-site

--- Docs/ans.py
+++
@@ -1,6 +0,0 @@
-for l in range(1, 1000):
-  print(' --- --- l = ' + str(l) + ' --- ---')
-  elems = []
-  for n in range(1, l + 1):
-    if (l % n == 0): elems.append(n)
-  print(2**(len(elems)), elems)
